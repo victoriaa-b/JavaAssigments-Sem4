b49a6f171fe48eda6a559f79ae71f18c2a7338a2
Victoria Breen <victoriabreen8@hotmail.com>
2025-01-13 16:32:08 -0330

    Doing question 1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Calculate the average temperature, and give how many days are above the average temperature
+// 1. Take an input from the user (eg 5)
+// 2. Prompt the user to enter all the 5 numbers (temperature values)
+// 3. Calculate the average temperature
+// 4. Given the average temperature, how many of the numbers in the line 2 are above the average temperature?
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+
+      // 1. Take the input from the user
+        // Need the number of days to work with
+        System.out.println("Please enter the total amount of days: ");
+        int totalNumOfDays = input.nextInt();
+
+        // array for the temps
+        int[] temps = new int[totalNumOfDays];
+        int sum = 0;
+
+
+    }
+}
