1b544f27203a0e505300712eba23c9af9e19bc4b
Victoria Breen <victoriabreen8@hotmail.com>
2025-01-20 19:29:36 -0330

    Starting new assigment

---
+++ Arrays/movieTheater.java
@@ -0,0 +1,8 @@
+// Simulate a movie theater that helps users to book and reserve seats
+// reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+// cancel seats
+// retrieve initial seating charting
+
+
+public class movieTheater {
+}
