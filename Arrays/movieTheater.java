b98e7ec7df4d13f5c077b70028624689f8e1604f
Victoria Breen <victoriabreen8@hotmail.com>
2025-01-21 16:33:59 -0330

    Finishing assignment 2

--- Arrays/movieTheater.java
+++ Arrays/movieTheater.java
@@ -17,7 +17,15 @@ public class movieTheater {
     }
 
     // Seating chart logic
-
+    public void seatingChart() {
+        System.out.println();
+        for (int[] row : seats) {  // 0 - available, 1 - taken
+            for (int seat : row) {
+                System.out.print(seat + " ");
+            }
+            System.out.println();
+        }
+    }
 
     // Checks to see of seats are in the parameters
     private boolean seatNum(int row, int column){
@@ -27,17 +35,25 @@ public class movieTheater {
     // Lets a seat be suggested if picked one is already taken
     // Scan seats from the seating charts
     private void suggestedSeat(){
-
+        for (int row = 0; row < seats.length; row++) {
+            for (int column = 0; column < seats[row].length; column++) {
+                if (seats[row][column] == 0) { // check to see if seat is available
+                    System.out.println("Try seat: Row " + row + ", Column " + column);
+                    return;
+                }
+            }
+        }
+        System.out.println("There are no seats currently available.");
     }
 
 
     // Reserving seats
     public void reservedSeat(int row, int column) {
         if (!seatNum(row, column)) {
-            System.out.println("Seat number does not exist.");
+            System.out.println("The Seat number does not exist.");
         } else if (seats[row][column] == 1){
-            System.out.println("Seat is already taken.");
-            suggestedSeat(); // make class
+            System.out.println("This seat is unavailable.");
+            suggestedSeat();
         } else {
             seats[row][column] = 1; // 1 sets the seat as reserved
             System.out.println("Your seat has been reserved.");
@@ -45,6 +61,39 @@ public class movieTheater {
     }
 
     // Cancelling Seats
+    public void cancellingSeat( int row, int column) {
+        if (!seatNum(row, column)) {
+            System.out.println("The Seat number does not exist.");
+        } else if (seats[row][column] == 0) {
+            System.out.println("This seat is not reserved.");
+        } else {
+            seats[row][column] = 0;
+            System.out.println("The reservation has been cancelled.");
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        movieTheater theaterSeating = new movieTheater(5, 5);
+
+        // Seating chart
+        System.out.println(("Seating Chart:"));
+        theaterSeating.seatingChart();
+
+        // Reserve a seat
+        System.out.println(("\nReserving seat (3,2):"));
+        theaterSeating.reservedSeat(3,2);
+        theaterSeating.seatingChart();
+
+        // reserve a taken one
+        System.out.println("\nTrying to reserve seat (3,2):");
+        theaterSeating.reservedSeat(3,2);
+
+        // Cancelling
+        System.out.println("\nCancelling seat (3,2)");
+        theaterSeating.cancellingSeat(3,2);
+        theaterSeating.seatingChart();
 
 
+}
 }
