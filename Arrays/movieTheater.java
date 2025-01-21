c9eb2921ea9fc6c6934634d30508ae796d4653f9
Victoria Breen <victoriabreen8@hotmail.com>
2025-01-20 21:39:04 -0330

    Adding constructor for assig 2

--- Arrays/movieTheater.java
+++ Arrays/movieTheater.java
@@ -1,8 +1,22 @@
 // Simulate a movie theater that helps users to book and reserve seats
-// reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+// (if seat is taken, let them know it is taken and suggest an available seat)
 // cancel seats
 // retrieve initial seating charting
 
 
 public class movieTheater {
+    public final int[][] seats; // 2 dimensional array
+
+    // Constructor & Initializing
+    public movieTheater(int rows, int columns){
+        seats = new int [rows][columns];
+        for(int[] row : seats) {
+            java.util.Arrays.fill(row,0); // 0 shows whether the seat is available or not
+        }
+
+    }
+
+    // Reserving seats
+
+
 }
