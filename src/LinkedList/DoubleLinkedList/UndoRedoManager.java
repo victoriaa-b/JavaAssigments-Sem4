87d9acd45082173d5674dc6e16dbd5a295aff17e
Victoria Breen <victoriabreen8@hotmail.com>
2025-01-31 18:31:12 -0330

    Finishing Assignment 4

--- src/LinkedList/DoubleLinkedList/UndoRedoManager.java
+++ src/LinkedList/DoubleLinkedList/UndoRedoManager.java
@@ -3,7 +3,7 @@ package LinkedList.DoubleLinkedList;
 
 // Assignment 4
 /**
- * Implememt an application that support undo/redo functionality. Use a linked list to maintain a sequence of states.\
+ * Implement an application that support undo/redo functionality. Use a linked list to maintain a sequence of states.\
  * Each state change is stored as a node in the list, allowing for easy navigation
  * 1<>2<>3<>4<>5
  */
@@ -27,10 +27,9 @@ public class UndoRedoManager<T> {
     private Node head;
 
     // Undo operation
-    // Check at method isn't working
     public T undo() {
         if (currentState != null && currentState.prev != null) {
-            currentState = currentState.next;
+            currentState = currentState.prev; // fixed line
             return currentState.state;
         }
         System.out.println("There is no previous state!");
@@ -39,18 +38,44 @@ public class UndoRedoManager<T> {
 
     // Perform an operation
     public void addState(T newState) {
-        //implement me
-
+        Node newNode = new Node(newState);
+        if (currentState != null) {
+            currentState.next = newNode;
+            newNode.prev = currentState; // Bring new node back to the current
+        } else {
+            head = newNode; // Node becomes the head
+        }
+    currentState = newNode;
     }
 
     //Redo Operation
     public T redo() {
-        //implement me
+        if ( currentState != null && currentState.next != null) {
+            currentState = currentState.next; // moves one state
+            return currentState.state;
+        }
+        System.out.println("There is no next state!");
+        return null;
 
 
     }
 
+    // Displays of the methods
     public static void main(String[] args) {
+        UndoRedoManager<String> manager = new UndoRedoManager<>();
+
+        // Add states - need 5
+        manager.addState("1");
+        manager.addState("2");
+        manager.addState("3");
+        manager.addState("4");
+        manager.addState("5");
+
+        // Print out Undo and Redo
+        String prev = manager.undo();
+        System.out.println(" Undo: " + prev);
+        String current = manager.redo();
+        System.out.println(" Redo: " + current);
 
     }
 }
