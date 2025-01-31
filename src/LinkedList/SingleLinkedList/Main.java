441e39c0503e9b043db3489dc755d6c3e5149854
Victoria Breen <victoriabreen8@hotmail.com>
2025-01-31 16:16:15 -0330

    Adding Boiler plate code and starting assignment 4

--- src/LinkedList/SingleLinkedList/Main.java
+++ src/LinkedList/SingleLinkedList/Main.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package LinkedList.SingleLinkedList;
 
 
 public class Main {
--- src/LinkedList/SingleLinkedList/SingleLinkedList.java
+++ src/LinkedList/SingleLinkedList/SingleLinkedList.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package LinkedList.SingleLinkedList;
 
 // In Class Code - Boiler Code
 public class SingleLinkedList {
